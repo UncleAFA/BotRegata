bb00b81bc2c50ced72fc44874cad8a6a7b6caeaa
Алексей Федоров <vedopov2014@gmail.com>
2022-08-30 12:33:27 +0300

    Добавьте файлы проекта.

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Telegram.Bot.Types;
+using BotRegata.Models;
+
+namespace BotRegata.Controllers
+{
+    [Route("api/message/update")]
+    public class MessageController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Method GET unuvalable";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<OkResult> Post([FromBody] object update)
+        {
+            var upd = JsonConvert.DeserializeObject<Update>(update.ToString());
+            if (upd == null) return Ok();
+            if (upd.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var commands = Bot.Commands;
+                var message = upd.Message;
+                var botClient = await Bot.GetBotClientAsync();
+
+                foreach (var command in commands)
+                {
+                    if (command.Contains(message))
+                    {
+                        await command.Execute(message, botClient);
+                        break;
+                    }
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Models/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BotRegata.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://dotnetcore-herokuafa.herokuapp.com/{0}";
+        public static string Name { get; set; } = "AFA_BOT_TODO_BOT";
+        public static string Key { get; set; } = "2044514869:AAF9wjbsxGXl0SgkMSyeNxpKFRcL28OfFXQ";
+    }
+}
