52891bab3f4c61aa85ab2e1cfe4d99f85de69fcc
Алексей Федоров <vedopov2014@gmail.com>
2022-08-30 13:50:51 +0300

    смена

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -2,7 +2,7 @@
 {
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://regatabot.herokuapp.com/{0}";
+        public static string Url { get; set; } = "https://appnameafa-heroku-20.herokuapp.com/{0}";
         public static string Name { get; set; } = "AFA_BOT_TODO_BOT";
         public static string Key { get; set; } = "2044514869:AAF9wjbsxGXl0SgkMSyeNxpKFRcL28OfFXQ";
     }
