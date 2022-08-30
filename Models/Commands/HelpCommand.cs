20f4d3e235a52f7ea5415e6d0224c2f1e3d2720c
Алексей Федоров <vedopov2014@gmail.com>
2022-08-30 13:58:49 +0300

    Изменение

--- Models/Commands/HelpCommand.cs
+++ Models/Commands/HelpCommand.cs
@@ -19,7 +19,7 @@ namespace BotRegata.Models.Commands
         public override async Task Execute(Message message, TelegramBotClient botClient)
         {
             var chatId = message.Chat.Id;
-            await botClient.SendTextMessageAsync(chatId, "Hallo I'm ASP.NET Core Bot", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+            await botClient.SendTextMessageAsync(chatId, "Мои команды:\n Пока только \n /start \n /help", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
         }
     }
 }
